6fd513d6ac4f0b1368858d1d4e24dbd006e3ac3e
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-03-18 14:50:57 +0800

    Add Modals

--- src/login.jsx
+++ src/login.jsx
@@ -3,11 +3,11 @@ import { FaEye } from "react-icons/fa";
 import App from './App.jsx'
 import { useNavigate } from "react-router";
 
-function Login({ click, users}) {
+function Login({ click, users }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    
+
 
 
     const handleUsernameChange = (e) => {
@@ -18,7 +18,7 @@ function Login({ click, users}) {
         setPassword(e.target.value);
     };
 
-    const loginSubmit = (e) => {
+    const loginSubmit = (e, ) => {
         e.preventDefault();
         console.log("Username:", username);
         console.log("Password:", password);
@@ -30,42 +30,42 @@ function Login({ click, users}) {
         }
     };
 
-        return (
-            <div className="border-2 border-indigo-700 w-full h-full m-4 p-2 rounded-xl shadow-[10px_5px_100px_-15px_rgba(0,0,255,1.0)]">
-                <div className="w-full flex flex-col gap-2">
-                    <h1 className="text-lg font-bold underline decoration-sky-500">Login</h1>
-                    <form className="m-5 gap-5 flex flex-col">
+    return (
+        <div className="border-2 border-indigo-700 w-full h-full m-4 p-2 rounded-xl shadow-[10px_5px_100px_-15px_rgba(0,0,255,1.0)]">
+            <div className="w-full flex flex-col gap-2">
+                <h1 className="text-lg font-bold underline decoration-sky-500">Login</h1>
+                <form className="m-5 gap-5 flex flex-col">
+                    <input
+                        className="h-12 w-full rounded-xl p-5 mb-4 hover:shadow-[10px_5px_100px_-15px_rgba(0,0,255,1.0)]"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                    <div className="relative inline-block ">
+                        <FaEye className="absolute text-[28px] top-[11px] transform right-[10px] cursor-pointer" />
                         <input
                             className="h-12 w-full rounded-xl p-5 mb-4 hover:shadow-[10px_5px_100px_-15px_rgba(0,0,255,1.0)]"
-                            type="text"
-                            placeholder="Username"
-                            value={username}
-                            onChange={handleUsernameChange}
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
                         />
-                        <div className="relative inline-block ">
-                            <FaEye className="absolute text-[28px] top-[11px] transform right-[10px] cursor-pointer" />
-                            <input
-                                className="h-12 w-full rounded-xl p-5 mb-4 hover:shadow-[10px_5px_100px_-15px_rgba(0,0,255,1.0)]"
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={handlePasswordChange}
-                            />
-                        </div>
-                        <button className="font-bold bg-indigo-500 hover:bg-indigo-700 p-3 rounded-lg" onClick={(e) => loginSubmit(e)}>
-                            Login
-                        </button>
-                        <button
-                            className="font-bold bg-red-500 hover:bg-red-700 p-3 rounded-lg"
-                            type="button"
-                            onClick={click}
-                        >
-                            Sign Up
-                        </button>
-                    </form>
-                </div>
+                    </div>
+                    <button className="font-bold bg-indigo-500 hover:bg-indigo-700 p-3 rounded-lg" onClick={(e) => loginSubmit(e)}>
+                        Login
+                    </button>
+                    <button
+                        className="font-bold bg-red-500 hover:bg-red-700 p-3 rounded-lg"
+                        type="button"
+                        onClick={click}
+                    >
+                        Sign Up
+                    </button>
+                </form>
             </div>
-        );
+        </div>
+    );
 
 }
 
